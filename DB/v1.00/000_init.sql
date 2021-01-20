5997a9cbdd4ae3e011a246eae0e9b611bd919708
e.utkin <uu3474@gmail.com>
2021-01-20 22:00:08 +0300

    DB sheme v1

---
+++ DB/v1.00/000_init.sql
@@ -0,0 +1,22 @@
+create table users (
+	id bigint primary key,
+	create_time timestamptz not null default(current_time()),
+	first_name text,
+	last_name text,
+	user_name text,
+	phone text
+);
+create index users_user_name_idx ON users (user_name);
+create index users_phone_idx ON users (phone);
+
+create table reviews (
+	id bigserial primary key,
+	create_time timestamptz not null default(current_time()),
+	from_user references users (id),
+	to_user references users (id),
+	grade smallint not null,
+	review text not null
+);
+create index reviews_create_time_idx ON reviews (create_time);
+create index reviews_from_user_idx ON reviews (from_user);
+create index reviews_to_user_idx ON reviews (to_user);
